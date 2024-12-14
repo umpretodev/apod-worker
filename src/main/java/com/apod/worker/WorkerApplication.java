a09376218b623e3b29f01517ab71d6fa1dc9c493
Lebackrobot <lebackman18@gmail.com>
2024-12-14 16:12:46 -0300

    feat(rabbitMq): enable

--- src/main/java/com/apod/worker/WorkerApplication.java
+++ src/main/java/com/apod/worker/WorkerApplication.java
@@ -1,6 +1,7 @@
 package com.apod.worker;
 
 import jakarta.annotation.PostConstruct;
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
